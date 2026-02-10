01f932895f22ece3e38992b5241183713ba48270
divyasonla <divyasonla23@navgurukul.org>
2026-02-10 10:13:23 +0530

    updated host in app.py

--- app.py
+++ app.py
@@ -320,4 +320,4 @@ def delete_weekly(row):
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))  # Default to 5000 if PORT is not set
-    app.run(debug=True, port=port)
+    app.run(debug=True, host="0.0.0.0", port=port)
